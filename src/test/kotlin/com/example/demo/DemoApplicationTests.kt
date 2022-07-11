49d39140f6737fe8d3ddebc5d118b1185d09c395
easywaldo <easywaldoji@gmail.com>
2022-07-11 23:34:30 +0900

    if expression

--- src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -16,6 +16,15 @@ class DemoApplicationTests {
 		val nullableStr: String? = null
 		val nullableStrLength = nullableStr?.length ?: "null 인 경우 반환".length
 		assert(nullableStrLength > 0)
+
+		val age: Int = 10
+		val str = if (age > 10) {
+			"old"
+		} else {
+			"new"
+		}
+		println(str)
+
 	}
 
 }
