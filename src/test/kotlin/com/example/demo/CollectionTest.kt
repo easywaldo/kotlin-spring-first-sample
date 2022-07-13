8cb013aa5e016daccdddb85f453f5d2081afb247
easywaldo <easywaldoji@gmail.com>
2022-07-13 23:28:28 +0900

    mutable list and set, map

--- src/test/kotlin/com/example/demo/CollectionTest.kt
+++ src/test/kotlin/com/example/demo/CollectionTest.kt
@@ -15,6 +15,35 @@ class CollectionTest {
         mutableCurrencyList.addAll(listOf("달러", "유로", "원"))
         mutableCurrencyList.add("위안")
         mutableCurrencyList.add("엔")
+
+        val mutableCurrencySecondList = mutableListOf<String>().apply{
+            add("달러")
+            add("위완")
+            add("원")
+        }
+
+        // immutable set
+        val numberSet = setOf<String>("one", "two", "one", "three")
+
+        // mutable set
+        val mutableSet = mutableSetOf<String>("one", "two", "three", "one", "two")
+        val mutableSet2 = mutableSetOf<Int>().apply {
+            add(1)
+            add(2)
+            add(3)
+            add(1)
+            add(2)
+        }
+
+        // immutable map
+        val numberMap = mapOf("one" to 1, "two" to 2)
+
+        // mutable map
+        val mutableNumberMap = mutableMapOf<String, Int>()
+        mutableNumberMap["one"] = 1
+        mutableNumberMap["two"] = 2
+        mutableNumberMap["three"] = 3
+
     }
 }
 
