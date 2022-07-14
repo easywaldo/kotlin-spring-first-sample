e4796382a6175517c677e284b01b7cf65b1fc191
easywaldo <easywaldoji@gmail.com>
2022-07-14 23:36:08 +0900

    immutable object copy

--- src/test/kotlin/com/example/demo/DataTest.kt
+++ src/test/kotlin/com/example/demo/DataTest.kt
@@ -21,9 +21,10 @@ class DataTest {
         // data class 가 아니라면 toString 구현을 해주어야 함
         println(person1.toString())
 
-        person2.name = "delta"
-        println(set.contains(person2))
+        val personCopied = person1.copy(name = "delta")
+        println(personCopied.name)
+        println(person1.name)
     }
 }
 
-data class Person(var name: String, val age: Int)
+data class Person(val name: String, val age: Int)
