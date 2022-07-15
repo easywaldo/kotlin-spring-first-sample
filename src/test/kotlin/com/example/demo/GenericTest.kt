1347189f2bcbcdc25349823a0e8d56e79628478e
easywaldo <easywaldoji@gmail.com>
2022-07-16 01:38:56 +0900

    반공변성 in keyword

--- src/test/kotlin/com/example/demo/GenericTest.kt
+++ src/test/kotlin/com/example/demo/GenericTest.kt
@@ -16,14 +16,24 @@ class GenericTest {
         val list4 : List<*> = listOf<Int>(1,2,3,4)
 
         val g1: List<String> = mutableListOf("test")
-        val g2: List<CharSequence> = g1 // 반공변성
+        val g2: List<CharSequence> = g1 // 공변성
 
-
-
+        val bag = Bag<String>()
+        bag.saveAll(
+            // 반공변성을 해결하기 위해서 in keyword 사용
+            mutableListOf<CharSequence>("1", "2"), mutableListOf<String>("3", "4"))
 
     }
 }
 
 class MyGeneric<T>(val t: T) {
 
+}
+
+class Bag<T> {
+    fun saveAll(
+        to: MutableList<in T>,
+        from: MutableList<T>,) {
+        to.addAll(from)
+    }
 }
