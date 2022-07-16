29bc9ce29016780ab180a46ea30bf03e7aa6b9b7
jinam.lee <jinam.lee@a-bly.com>
2022-07-16 12:10:35 +0900

    lazy binding

---
+++ src/test/kotlin/com/example/demo/LazyBindingTest.kt
@@ -0,0 +1,26 @@
+package com.example.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LazyBindingTest {
+    @Test
+    fun test() {
+        val helloBot = HelloBot()
+        helloBot.sayHello()
+        helloBot.sayHello()
+        helloBot.sayHello()
+    }
+}
+
+class HelloBot {
+    val greeting: String by lazy {
+        println("초기화 로직 수행")
+        getHello()
+    }
+
+    fun sayHello() = println(greeting)
+}
+
+fun getHello() = "안녕하세요"
