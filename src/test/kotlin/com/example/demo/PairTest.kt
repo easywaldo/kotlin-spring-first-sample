9221a0cb4bff3082cd04a516007a5f89fdb01013
easywaldo <easywaldoji@gmail.com>
2022-07-17 12:53:06 +0900

    Pair class, Triple Class

--- src/test/kotlin/com/example/demo/PairTest.kt
+++ src/test/kotlin/com/example/demo/PairTest.kt
@@ -8,11 +8,38 @@ class PairTest {
 
     @Test
     fun test() {
-        val plus = plus(Tuple(1, 3))
-        println(plus)
+//        val plus = plus(Tuple(1, 3))
+//        println(plus)
+
+        val pair = plus(Pair(1, 100))
+        println(pair)
+
+        val pair2 = Pair("A", 3)
+        val newPair = pair2.copy(first = "B")
+        println(newPair)
+
+        val second = newPair.component2()
+       println(second)
+
+       val list = newPair.component2()
+       println(list)
+
+       val triple = Triple("A", "B", "C")
+        println(triple)
+
+        triple.first
+        triple.second
+        val newTriple = triple.copy(third = "Z")
+        println(newTriple)
+
+        println(newTriple.component3())
+
+
     }
 }
 
 data class Tuple(val a: Int, val b: Int)
 
-fun plus(tuple: Tuple) = tuple.a + tuple.b
+//fun plus(tuple: Tuple) = tuple.a + tuple.b
+
+fun plus(pair: Pair<Int, Int>) = pair.first + pair.second
