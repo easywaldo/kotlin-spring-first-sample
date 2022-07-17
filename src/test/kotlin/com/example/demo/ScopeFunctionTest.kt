a9291e559c51d28dd596b1baab10b97e8baf99f2
easywaldo <easywaldoji@gmail.com>
2022-07-17 13:20:39 +0900

    run scope function

--- src/test/kotlin/com/example/demo/ScopeFunctionTest.kt
+++ src/test/kotlin/com/example/demo/ScopeFunctionTest.kt
@@ -23,6 +23,38 @@ class ScopeFunctionTest {
             1234
         }
 
+//        val config = DatabaseClient()
+//        config.url = "localhost:3306"
+//        config.username = "mysql"
+//        config.password = "1234"
+//        val connected = config.connect()
+//        println(connected)
+
+        // run scope function
+        val connected = DatabaseClient().run {
+            url = "localhost:3306"
+            username = "mysql"
+            password = "1234"
+            connect()
+        }
+        println(connected)
+
+
+
         println(result)
+    }
+}
+
+
+class DatabaseClient {
+    var url: String? = null
+    var username: String? = null
+    var password: String? = null
+
+    fun connect(): Boolean {
+        println("DB 접속 중 ...")
+        Thread.sleep(1000)
+        println("DB 접속 완료")
+        return true
     }
 }
