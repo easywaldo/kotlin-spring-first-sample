96ce84577c380a92d849264f874877f2b7ed880f
easywaldo <easywaldoji@gmail.com>
2022-07-19 23:57:11 +0900

    lambda sample

---
+++ src/test/kotlin/com/example/demo/LambdaTest.kt
@@ -0,0 +1,19 @@
+package com.example.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LambdaTest {
+
+    val func: () -> Unit = {}
+    val strFunc: () -> String = {""}
+    val printHello: () -> Unit = { println("hello") }
+    val list = mutableListOf(printHello)
+
+    @Test
+    fun test() {
+        println(list[0]())
+    }
+}
+
