3a0b3b0615558af51f9f22bd995ee377301bbef9
easywaldo <easywaldoji@gmail.com>
2022-07-20 00:30:34 +0900

    lambda function style

--- src/test/kotlin/com/example/demo/LambdaTest.kt
+++ src/test/kotlin/com/example/demo/LambdaTest.kt
@@ -51,6 +51,26 @@ class LambdaTest {
         val printStr: (String) -> Unit = { println(it) }
         forEachStr(list, printStr)
 
+        // foreach 도 고차함수
+        list.forEach{
+            println(it)
+            printStr
+        }
+
+        val upperCase: (String) -> Unit = { it.uppercase() }
+        val mapResult = list.map {
+            upperCase
+        }
+
+
+        outerFunc()()
+        val outerF = outerFunc()
+        outerFunc()
+
+        outerFunc2()()
+
+        println(sum(1, 1))
+        println(sum2(1, 1))
     }
 
     fun forEachStr(collection: Collection<String>, action: (String) -> Unit) {
@@ -58,5 +78,20 @@ class LambdaTest {
             action(item)
         }
     }
+
+    fun outerFunc() : () -> Unit {
+        return fun() {
+            println("this is anonymous function")
+        }
+    }
+
+    fun outerFunc2() : () -> Unit {
+        return {
+            println("this is anonymous function2222")
+        }
+    }
+
+    val sum = { x: Int, y: Int -> x + y }
+    val sum2: (Int, Int) -> Int = { x, y -> x + y}
 }
 
