5512e5a53133ffb72e32e66b83108057eeba4835
easywaldo <easywaldoji@gmail.com>
2022-07-20 23:41:26 +0900

    Precondition function - require

---
+++ src/test/kotlin/com/example/demo/PreconditionTest.kt
@@ -0,0 +1,29 @@
+package com.example.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PreconditionTest {
+    @Test
+    fun test() {
+        obtainQuest(100)
+        obtainQuest(0)
+    }
+
+    private fun obtainQuest(
+        playerLevel: Int,
+        playerClass: String = "paladin",
+        hasBefriendedBarbarians: Boolean = true,
+        hasAngeredBarbarians: Boolean = false) : String? {
+        require(playerLevel > 0) {
+            "The player's level must be at least 1."
+        }
+
+        return when (playerLevel) {
+            1 -> "go to the town"
+            2 -> "learn basic magic skill"
+            else -> return null
+        }
+    }
+}
