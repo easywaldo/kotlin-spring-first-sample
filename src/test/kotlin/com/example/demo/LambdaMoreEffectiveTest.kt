6bf32585579c9f92ec4fbb503dc297296446984d
jinam.lee <jinam.lee@a-bly.com>
2022-07-23 12:53:12 +0900

    more effective lambda test

---
+++ src/test/kotlin/com/example/demo/LambdaMoreEffectiveTest.kt
@@ -0,0 +1,61 @@
+package com.example.demo
+
+import org.junit.jupiter.api.RepeatedTest
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+@SpringBootTest
+class LambdaMoreEffectiveTest {
+    var narrationModifier: (String) -> String = {
+        it
+    }
+
+    fun narrate(message: String) {
+        println(narrationModifier(message))
+    }
+
+    fun changeNarratorMood() {
+        val mood: String
+        val modifier: (String) -> String
+
+        when (Random.nextInt(1..4)) {
+            1 -> {
+                mood = "loud"
+                modifier = {
+                    message ->
+                        val numExclamationPoints = 3
+                        message.uppercase() + "!".repeat(numExclamationPoints)
+                }
+            }
+            2 -> {
+                mood = "tired"
+                modifier = {
+                    message ->
+                        message.lowercase().replace(" ", "...")
+                }
+            }
+            3 -> {
+                mood = "unsure"
+                modifier = {
+                    message -> "$message?"
+                }
+            }
+            else -> {
+                mood = "professional"
+                modifier = {
+                    message -> "$message."
+                }
+            }
+        }
+
+        narrationModifier = modifier
+        narrate("The narrator begins to feel $mood")
+    }
+
+    @RepeatedTest(value = 8)
+    fun test() {
+        changeNarratorMood()
+    }
+}
