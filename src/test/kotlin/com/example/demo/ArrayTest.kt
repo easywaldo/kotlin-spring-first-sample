6de92a516b38118bf867c4182e9507c01048d00a
easywaldo <easywaldoji@gmail.com>
2022-07-30 12:09:22 +0900

    listOf add equals test

--- src/test/kotlin/com/example/demo/ArrayTest.kt
+++ src/test/kotlin/com/example/demo/ArrayTest.kt
@@ -8,6 +8,13 @@ class ArrayTest {
     @Test
     fun test() {
         displayPlayerAges(intArrayOf(20, 19, 25, 23, 43, 32, 39, 23))
+        var x = listOf(mutableListOf(1,2,3))
+        var y = listOf(mutableListOf(1,2,3))
+
+        assert(x == y)
+        x[0].add(3)
+
+        assert(x == y)
     }
 
     fun displayPlayerAges(playerAges: IntArray) {
