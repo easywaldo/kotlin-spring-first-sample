7dde6337c8c3863e97fd22af2d335cec66abcc22
easywaldo <easywaldoji@gmail.com>
2022-08-02 23:45:59 +0900

    read text using map

---
+++ src/test/kotlin/com/example/demo/TransformingDataMapTest.kt
@@ -0,0 +1,20 @@
+package com.example.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import java.io.File
+
+@SpringBootTest
+class TransformingDataMapTest {
+    @Test
+    fun test() {
+        val menuData = File("data/menu-data.txt")
+            .readText()
+            .split("\n")
+
+        val menuItems = List(menuData.size) {
+            index -> val (_, name, _) = menuData[index].split(",")
+            println(name)
+        }
+    }
+}
