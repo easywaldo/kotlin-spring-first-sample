db7e881d2dcfe6907025651e3701640951efe9e8
easywaldo <easywaldoji@gmail.com>
2022-08-17 23:25:13 +0900

    Professor class added in lecture package

---
+++ src/main/kotlin/com/example/demo/domain/lecture/Professor.kt
@@ -0,0 +1,27 @@
+package com.example.demo.domain.lecture
+
+class Professor {
+    var name = "easywaldo"
+    get() = field.replaceFirstChar { it.uppercase() }
+    private set(value) {
+        field = value.trim()
+    }
+
+    val title: String
+        get() = when {
+            name.all { it.isDigit() } -> "The Identifiable"
+            name.none { it.isLetter() } -> "The Witness Protection Member"
+            name.count { it.lowercase() in "aeiou" } > 4 ->
+                "The Master of Vowels"
+            else -> "The Renowned Professor"
+        }
+
+    fun pointBlackboard(countOfPointing: Int = 2) {
+        println("pointing blackboard front of students")
+    }
+
+    fun changeName(newName: String) {
+        println("need to change name")
+        name = newName
+    }
+}
