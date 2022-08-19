d179aa5d7ef3a5ac92f8c6da7e1509fdb14cd77b
easywaldo <easywaldoji@gmail.com>
2022-08-19 19:21:17 +0900

    Lateinit test

--- src/test/kotlin/com/example/demo/LateInitTest.kt
+++ src/test/kotlin/com/example/demo/LateInitTest.kt
@@ -7,31 +7,39 @@ import org.springframework.boot.test.context.SpringBootTest
 class LateInitTest {
     @Test
     fun test() {
-        var lateInit: LateInit = LateInit()
-        lateInit.text = "hello"
-        println(lateInit.text)
+        val arena = Arena()
+        arena.prepareArena()
+        println(arena.opponentName)
 
-        println(lateInit.textInitialized)
-        println("=========================")
-
-        val lateInit2 = LateInit()
-        lateInit2.printText()
-        lateInit2.printText()
+        arena.prophesize()
+        println("time is going....")
+        println("time is going....")
+        arena.prophesize()
     }
 }
 
-class LateInit {
-    lateinit var text: String
-    val textInitialized: Boolean
-        get() = this::text.isInitialized
+class Arena {
+    var isArenaOpen = false
+    lateinit var opponentName: String
 
-    fun printText() {
-        if (this::text.isInitialized) {
-            println("initialized..")
-        } else {
-            println("not initialized..")
-            text = "안녕하세요"
-        }
-        println(text)
+    fun prepareArena() {
+        isArenaOpen = true
+        opponentName = getWillingCombatants().random()
+    }
+
+    private fun getWillingCombatants() = listOf<String>("Cornelius", "Cheryl", "Ralph", "Deborah")
+
+    val prophecy by lazy {
+        Thread.sleep(3000)
+        "An intrepid hero from hometown shall some day " + listOf(
+            "form an unlikely bond between two warring factions",
+            "take possession of an otherworldly blade",
+            "bring the gift of creation back to the world",
+            "best the world-eater"
+        ).random()
+    }
+
+    fun prophesize() {
+        println("A fortune teller told Madrigal, $prophecy")
     }
 }
