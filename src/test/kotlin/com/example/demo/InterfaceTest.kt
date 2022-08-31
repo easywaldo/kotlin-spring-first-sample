010cd3a7bfc9657f48f803e06d13bbf608734e19
easywaldo <easywaldoji@gmail.com>
2022-09-01 01:27:09 +0900

    Implementing interface

--- src/test/kotlin/com/example/demo/InterfaceTest.kt
+++ src/test/kotlin/com/example/demo/InterfaceTest.kt
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest
 class InterfaceTest {
     @Test
     fun test() {
-
+        val gamePlayer1: Fightable = GamePlayer(initialName = "easywaldo", homeTown = "Seoul", healthPoints = 300, isImmortal = true)
+        val gamePlayer2: Fightable = GamePlayer(initialName = "dodo", homeTown = "NewYork", healthPoints = 300, isImmortal = true)
     }
 }
 
@@ -19,4 +20,33 @@ interface Fightable {
 
     fun takeDamage(damage: Int)
     fun attack(opponent: Fightable)
+}
+
+class GamePlayer(
+    initialName: String,
+    val homeTown: String = "Nebraska",
+    override var healthPoints: Int,
+    val isImmortal: Boolean,
+): Fightable {
+
+    override var name = initialName
+        get() = field.replaceFirstChar { it.uppercaseChar() }
+        private set(value) {
+            field = value.trim()
+        }
+
+    override val diceCount: Int
+        get() = TODO("Not yet implemented")
+
+    override val diceSides: Int
+        get() = TODO("Not yet implemented")
+
+    override fun takeDamage(damage: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun attack(opponent: Fightable) {
+        TODO("Not yet implemented")
+    }
+
 }
