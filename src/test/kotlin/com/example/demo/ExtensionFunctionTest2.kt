ad00f4f7d4366df3c087ab766419a66d499eb1fe
easywaldo <easywaldoji@gmail.com>
2022-09-06 23:52:05 +0900

    Extending Any

--- src/test/kotlin/com/example/demo/ExtensionFunctionTest2.kt
+++ src/test/kotlin/com/example/demo/ExtensionFunctionTest2.kt
@@ -8,8 +8,14 @@ class ExtensionFunctionTest2 {
     @Test
     fun test() {
         println("HelloWorld".addEnthusiasm(5))
+
+        5.print()
     }
 }
 
 fun String.addEnthusiasm(enthusiasmLevel: Int = 1) =
     this + "!".repeat(enthusiasmLevel)
+
+fun Any.print() {
+    println(this)
+}
