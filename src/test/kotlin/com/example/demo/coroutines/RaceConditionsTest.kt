8ea99ba827854e9d670eccc251895635b13e6f98
easywaldo <easywaldoji@gmail.com>
2022-09-11 11:26:48 +0900

    Race condition

---
+++ src/test/kotlin/com/example/demo/coroutines/RaceConditionsTest.kt
@@ -0,0 +1,27 @@
+package com.example.demo.coroutines
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RaceConditionsTest {
+    val passengersPerFlight = 75
+    val numberOfFlights = 1000
+    var checkedPassengers = 0
+
+    @Test
+    fun test() {
+        repeat(numberOfFlights) {
+            runBlocking {
+                launch(Dispatchers.Default) {
+                    checkedPassengers += passengersPerFlight
+                }
+            }
+        }
+
+        println(checkedPassengers)
+    }
+}
