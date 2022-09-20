c9b145b58681c0ca9128c96e2f551448b4ca9520
easywaldo <easywaldoji@gmail.com>
2022-09-20 23:59:11 +0900

    function passing parameter

--- src/test/kotlin/com/example/demo/HigherFunctionTest.kt
+++ src/test/kotlin/com/example/demo/HigherFunctionTest.kt
@@ -8,18 +8,24 @@ class HigherFunctionTest {
     @Test
     fun test() {
         var total: Int = 0
-        println(measureTime {
-            for (i in 1 .. 10000) {
-                total += 10
-            }
-        })
+        println(measureTime({
+            sumTest()
+            sumTest()
+        }))
     }
 
-    fun measureTime(action: (() -> Unit)?): Long {
+    fun measureTime(action: ()->Unit): Long {
         val start = System.nanoTime()
 
         action?.invoke()
 
         return System.nanoTime() - start
+    }
+
+    fun sumTest() {
+        var total: Int = 0
+        for (i in 1 .. 10000) {
+            total += 10
+        }
     }
 }
