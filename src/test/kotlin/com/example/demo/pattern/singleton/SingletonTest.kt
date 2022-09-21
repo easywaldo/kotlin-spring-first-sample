5d3ceff695b194e8d5fd23e867bfa1a7eb0bc39d
easywaldo <easywaldoji@gmail.com>
2022-09-21 23:57:49 +0900

    Static factory method

---
+++ src/test/kotlin/com/example/demo/pattern/singleton/SingletonTest.kt
@@ -0,0 +1,24 @@
+package com.example.demo.pattern.singleton
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SingletonTest {
+    @Test
+    fun test() {
+        // static factoy method
+        Server.withPort(8080)
+
+        val server = Server(port = 8088)
+    }
+}
+
+class Server private constructor(port: Long) {
+    init {
+        println("Server started on port $port")
+    }
+    companion object {
+        fun withPort(port: Long) = Server(port)
+    }
+}
