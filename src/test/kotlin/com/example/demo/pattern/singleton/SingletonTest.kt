8bb79881f1560dbae38a810c8946cf83d60091ac
easywaldo <easywaldoji@gmail.com>
2022-09-22 00:25:00 +0900

    kotlin object type - as a singleton instance

--- src/test/kotlin/com/example/demo/pattern/singleton/SingletonTest.kt
+++ src/test/kotlin/com/example/demo/pattern/singleton/SingletonTest.kt
@@ -6,11 +6,33 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class SingletonTest {
     @Test
-    fun test() {
+    fun static_factory_test() {
         // static factoy method
         Server.withPort(8080)
 
         // val server = Server(port = 8088)    // Doesn't compile
+    }
+
+    @Test
+    fun singleton_test() {
+        val myFavoriteQuickAndAngryMovies = NoMoviesList
+        val yourFavoriteQuickAndAngryMovies = NoMoviesList
+        // same instance
+        assert(myFavoriteQuickAndAngryMovies === yourFavoriteQuickAndAngryMovies)
+
+        val myFavoriteMovies = listOf("Black Hawk Down", "Blade   Runner")
+        printMovies(myFavoriteMovies)
+
+        printMovies(listOf())
+        printMovies(emptyList())
+
+        val bugetLogger = MoneyLogger
+        val bugetLogger2 = MoneyLogger
+        bugetLogger.useMoney(10)
+        bugetLogger2.useMoney(20)
+
+        println("remained money ${bugetLogger.printMoney()}")
+
     }
 }
 
@@ -20,5 +42,30 @@ class Server private constructor(port: Long) {
     }
     companion object {
         fun withPort(port: Long) = Server(port)
+    }
+}
+
+object NoMoviesList
+
+fun printMovies(movies: List<String>) {
+    for (m in movies) {
+        println(m)
+    }
+}
+
+
+object MoneyLogger {
+    var money: Int = 100
+    init {
+        println("I was accessed for the first time")
+        // Initialization logic goes here
+    }
+    // More code goes here
+    fun useMoney(cost: Int) {
+        money -= cost
+    }
+
+    fun printMoney(): Int {
+        return money
     }
 }
