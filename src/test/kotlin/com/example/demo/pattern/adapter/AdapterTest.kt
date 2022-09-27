2111d2c750171ba3516fb4761740ac98b19c68f6
easywaldo <easywaldoji@gmail.com>
2022-09-27 23:42:59 +0900

    Adapter pattern

--- src/test/kotlin/com/example/demo/pattern/adapter/AdapterTest.kt
+++ src/test/kotlin/com/example/demo/pattern/adapter/AdapterTest.kt
@@ -1,4 +1,73 @@
 package com.example.demo.pattern.adapter
 
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
 class AdapterTest {
+    @Test
+    fun test() {
+        val cellPhone = cellPhone(
+            charger(
+                usPowerOutlet().toEUPlug()
+            ).toUsbTypeC()
+        )
+    }
+}
+
+interface USPlug {
+    val hasPower: Int
+}
+
+interface EUPlug {
+    val hasPower: String // "TRUE" or "FALSE"
+}
+
+interface UsbMini {
+    val hasPower: Power
+}
+enum class Power {
+    TRUE, FALSE
+}
+
+interface UsbTypeC {
+    val hasPower: Boolean
+}
+
+fun cellPhone(chargeCable: UsbTypeC) {
+    if (chargeCable.hasPower) {
+        println("I've Got The Power!")
+    } else {
+        println("No power")
+    }
+}
+
+// Power outlet exposes USPlug interface
+fun usPowerOutlet(): USPlug {
+    return object : USPlug {
+        override val hasPower = 1
+    }
+}
+
+// Charger accepts EUPlug interface and exposes UsbMini
+// interface
+fun charger(plug: EUPlug): UsbMini {
+    return object : UsbMini {
+        override val hasPower=Power.valueOf(plug.hasPower)
+    }
+}
+
+fun USPlug.toEUPlug(): EUPlug {
+    val hasPower = if (this.hasPower == 1) "TRUE" else       "FALSE"
+    return object : EUPlug {
+        // Transfer power
+        override val hasPower = hasPower
+    }
+}
+
+fun UsbMini.toUsbTypeC(): UsbTypeC {
+    val hasPower = this.hasPower == Power.TRUE
+    return object : UsbTypeC {
+        override val hasPower = hasPower
+    }
 }
