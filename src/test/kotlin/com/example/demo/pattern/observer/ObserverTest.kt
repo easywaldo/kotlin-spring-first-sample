4ca33b76de058c82fe70b08a9b5485038f4549cd
easywaldo <easywaldoji@gmail.com>
2022-10-11 23:29:21 +0900

    joinChoir function modified

--- src/test/kotlin/com/example/demo/pattern/observer/ObserverTest.kt
+++ src/test/kotlin/com/example/demo/pattern/observer/ObserverTest.kt
@@ -11,23 +11,32 @@ class ObserverTest {
         val bat = Bat()
         val dog = Dog()
         val turkey = Turkey()
-        catTheConductor.joinChoir(bat::screech)
-        catTheConductor.joinChoir(dog::howl)
-        catTheConductor.joinChoir(dog::bark)
-        catTheConductor.joinChoir(turkey::gobble)
+        catTheConductor.joinChoir(bat.screech(HighMessage(repeat = 3)))
+        catTheConductor.joinChoir(dog.howl())
+        catTheConductor.joinChoir(dog.bark())
+        catTheConductor.joinChoir(turkey.gobble(repeat = 5))
 
-        catTheConductor.leaveChoir(bat::screech)
+        catTheConductor.leaveChoir(bat.screech(LowMessage(repeat = 2)))
     }
 }
 
 class Bat {
-    fun screech() {
-        println("Eeeeeee")
+    fun screech(message: Message) {
+        when (message) {
+            is HighMessage -> {
+                for (i in 1..message.repeat) {
+                    println("${message.pitch} Eeeeeee")
+                }
+            }
+            else -> println("Can't :(")
+        }
     }
 }
 class Turkey {
-    fun gobble() {
-        println("Gob-gob")
+    fun gobble(repeat: Times) {
+        for (i in 1..repeat) {
+            println("Gob-gob")
+        }
     }
 }
 class Dog {
@@ -41,10 +50,33 @@ class Dog {
 
 class Cat {
     private val participants = mutableMapOf<()->Unit, ()->Unit>()
-    fun joinChoir(whatToCall: ()->Unit) {
-        participants[whatToCall] = whatToCall
+    fun joinChoir(whatToCall: Unit) {
+        participants[{ whatToCall }] = { whatToCall }
     }
-    fun leaveChoir(whatNotToCall: ()->Unit) {
-        participants.remove(whatNotToCall)
+    fun leaveChoir(whatNotToCall: Unit) {
+        participants.remove { whatNotToCall }
     }
+    fun conduct(n: Times) {
+        for (p in participants.values) {
+            for (i in 1..n) {
+                p()
+            }
+        }
+    }
+}
+
+typealias Times = Int
+
+enum class SoundPitch {HIGH, LOW}
+
+
+interface Message {
+    val repeat: Times
+    val pitch: SoundPitch
+}
+data class LowMessage(override val repeat: Times) : Message {
+    override val pitch = SoundPitch.LOW
+}
+data class HighMessage(override val repeat: Times) : Message {
+    override val pitch = SoundPitch.HIGH
 }
