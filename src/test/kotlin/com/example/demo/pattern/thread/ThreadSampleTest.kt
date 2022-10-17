371a842e7816fbfc4ac8fe060f7bc31b9f41fb39
easywaldo <easywaldoji@gmail.com>
2022-10-17 23:46:55 +0900

    another way of thread sample

--- src/test/kotlin/com/example/demo/pattern/thread/ThreadSampleTest.kt
+++ src/test/kotlin/com/example/demo/pattern/thread/ThreadSampleTest.kt
@@ -2,19 +2,29 @@ package com.example.demo.pattern.thread
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import kotlin.concurrent.thread
 
 @SpringBootTest
 class ThreadSampleTest {
     @Test
     fun test() {
         for (t in 0..1) {
-            var finalT = t
             Thread {
                 for (i in 0..99) {
-                    finalT += i
                     println("Thread $t: $i")
                 }
             }.start()
+        }
+    }
+
+    @Test
+    fun test2() {
+        repeat(2) { t ->
+            thread {
+                for (i in 1..100) {
+                    println("T$t: $i")
+                }
+            }
         }
     }
 }
