88cdc57e07ac65ce8ce006dad76bf01fb35aa5d6
easywaldo <easywaldoji@gmail.com>
2022-10-24 23:58:18 +0900

    Flow sample

--- src/test/kotlin/com/example/demo/pattern/channel/ChannelTest.kt
+++ src/test/kotlin/com/example/demo/pattern/channel/ChannelTest.kt
@@ -2,6 +2,9 @@ package com.example.demo.pattern.channel
 
 import com.example.demo.concurrent.launch
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import org.apache.tomcat.jni.Socket.send
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -19,6 +22,16 @@ class ChannelTest {
                 chan.send(it)
             }
             chan.close()
+        }
+    }
+
+    @Test
+    fun test2() {
+        val numbersFlow: Flow<Int> = flow {
+            (0..10).forEach {
+                println("Sending $it")
+                emit(it)
+            }
         }
     }
 }
