7401e32d9e307dc7fc33aa23872746a31de5f218
easywaldo <easywaldoji@gmail.com>
2022-10-25 00:34:18 +0900

    Buffering flow

--- src/test/kotlin/com/example/demo/pattern/channel/ChannelTest.kt
+++ src/test/kotlin/com/example/demo/pattern/channel/ChannelTest.kt
@@ -4,6 +4,7 @@ import com.example.demo.concurrent.launch
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.flow
 import org.apache.tomcat.jni.Socket.send
 import org.junit.jupiter.api.Test
@@ -62,6 +63,24 @@ class ChannelTest {
                         delay(1000)
                         println("Coroutine $coroutineId received $number")
                     }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun buffering_flow() {
+        val numbersFlow: Flow<Int> = flow {
+            (0..10).forEach {
+                println("Sending $it")
+                emit(it)
+            }
+        }
+        runBlocking {
+            (1..4).forEach { coroutineId ->
+                numbersFlow.buffer().collect { number ->
+                    delay(1000)
+                    println("Coroutine $coroutineId received $number")
                 }
             }
         }
