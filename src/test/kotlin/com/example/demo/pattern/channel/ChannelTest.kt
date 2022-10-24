b831a4f307b2e25f70283b517507efc00bb76fad
easywaldo <easywaldoji@gmail.com>
2022-10-25 00:23:34 +0900

    launching flow in CoroutineDefaultScope

--- src/test/kotlin/com/example/demo/pattern/channel/ChannelTest.kt
+++ src/test/kotlin/com/example/demo/pattern/channel/ChannelTest.kt
@@ -1,10 +1,10 @@
 package com.example.demo.pattern.channel
 
 import com.example.demo.concurrent.launch
+import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.runBlocking
 import org.apache.tomcat.jni.Socket.send
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
@@ -42,6 +42,27 @@ class ChannelTest {
             }
             catch (e: Exception) {
                 println("Got an error")
+            }
+        }
+    }
+
+    @Test
+    fun test3() {
+        val numbersFlow: Flow<Int> = flow {
+            (0..10).forEach {
+                println("Sending $it")
+                emit(it)
+            }
+        }
+        runBlocking {
+            (1..4).forEach { coroutineId ->
+                delay(5000)
+                CoroutineScope(Dispatchers.Default).launch {
+                    numbersFlow.collect { number ->
+                        delay(1000)
+                        println("Coroutine $coroutineId received $number")
+                    }
+                }
             }
         }
     }
