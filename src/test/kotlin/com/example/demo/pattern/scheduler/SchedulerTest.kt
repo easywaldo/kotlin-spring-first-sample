7ee5de2c4eae487e8942af3e71b439fa4f1fd50a
easywaldo <easywaldoji@gmail.com>
2022-10-26 23:45:01 +0900

    pipeline sample test

--- src/test/kotlin/com/example/demo/pattern/scheduler/SchedulerTest.kt
+++ src/test/kotlin/com/example/demo/pattern/scheduler/SchedulerTest.kt
@@ -1,6 +1,7 @@
 package com.example.demo.pattern.scheduler
 
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.produce
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import java.util.concurrent.ForkJoinPool
@@ -46,6 +47,29 @@ class SchedulerTest {
                 }
             }
             forkJoinPool.close()
+        }
+    }
+
+    @Test
+    fun pipeline_test() {
+        runBlocking {
+            producePages()
+        }
+    }
+
+    fun CoroutineScope.producePages() = produce {
+        fun getPages(): List<String> {
+            return listOf(
+                "<html><body><h1>Cool stuff</h1></body></html>",
+                "<html><body><h1>Even more stuff</h1></body></html>"
+            )
+        }
+
+        val pages = getPages()
+        while (isActive) {
+            for (p in pages) {
+                send(p)
+            }
         }
     }
 }
