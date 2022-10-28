9f055a4075e03ec72abf800e4f92227b1fb3dfd5
easywaldo <easywaldoji@gmail.com>
2022-10-29 00:27:32 +0900

    BaseEntity added

--- src/main/kotlin/com/issue/demo/IssueServiceApplication.kt
+++ src/main/kotlin/com/issue/demo/IssueServiceApplication.kt
@@ -3,10 +3,12 @@ package com.issue.demo
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class IssueServiceApplication: CommandLineRunner {
-    override fun run(vararg args: String?) {
+    override fun run(vararg args: String?)  {
         println("hello world")
     }
 }
