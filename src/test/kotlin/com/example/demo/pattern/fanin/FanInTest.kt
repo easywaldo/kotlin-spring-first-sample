d6d56bfdd35611c18fbf0d2e77c77ce0a344288d
easywaldo <easywaldoji@gmail.com>
2022-10-28 23:56:17 +0900

    Coroutine fan in pattern

---
+++ src/test/kotlin/com/example/demo/pattern/fanin/FanInTest.kt
@@ -0,0 +1,37 @@
+package com.example.demo.pattern.fanin
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.produce
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FanInTest {
+    @Test
+    fun fan_in_test() {
+        runBlocking {
+            doWork()
+        }
+    }
+
+    fun CoroutineScope.generateWork() = produce {
+        for (i in 1..10_000) {
+            send("page$i")
+        }
+        close()
+    }
+    fun CoroutineScope.doWork(
+        id: Int,
+        channel: ReceiveChannel<String>) = launch(Dispatchers.Default) {
+        for (p in channel) {
+            println("Worker $id processed $p")
+        }
+    }
+    suspend fun doWork() = coroutineScope {
+        val workChannel = generateWork()
+        val workers = List(10) { id ->
+            doWork(id, workChannel)
+        }
+    }
+}
