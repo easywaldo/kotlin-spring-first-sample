f0fa6295385f516014e109605b50a5b84f157904
easywaldo <easywaldoji@gmail.com>
2022-11-13 22:01:38 +0900

    High order function sample

--- src/test/kotlin/com/example/demo/functional/FunctionalSampleTest.kt
+++ src/test/kotlin/com/example/demo/functional/FunctionalSampleTest.kt
@@ -47,10 +47,28 @@ class FunctionalSampleTest {
         }
         println(sample(1,2,3,4,5,6,7,8,9,10,11))
     }
+
+    @Test
+    fun high_order_function()  {
+        val numbers = listOf(1,2,3,4,5,6,7,8,9,10)
+        numbers.forEach {
+            println("Called with 4 : ${perf_operation_on_even(it, {it * 2})}")
+        }
+    }
+
+
 }
 
 fun String.countWords():Int {
     return trim()
         .split(Pattern.compile("\\s+"))
         .size
+}
+
+fun perf_operation_on_even(number: Int, operation: (Int) -> Int): Int {
+    if (number %2 == 0) {
+        return operation(number)
+    } else {
+        return number
+    }
 }
