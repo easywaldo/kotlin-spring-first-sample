8ac135a269686c6d99d9b2e682e7069c414241e5
easywaldo <easywaldoji@gmail.com>
2022-11-14 23:37:59 +0900

    코멘트 수정 도메인 적용

--- src/main/kotlin/com/issue/demo/domain/CommentRepository.kt
+++ src/main/kotlin/com/issue/demo/domain/CommentRepository.kt
@@ -3,4 +3,5 @@ package com.issue.demo.domain
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface CommentRepository : JpaRepository<Comment, Long> {
+    fun findByIdAndUserId(id: Long, userId: Long): Comment?
 }
