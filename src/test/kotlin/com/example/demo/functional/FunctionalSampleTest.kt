153802db290949ba67a147a47666763b0fd98c60
easywaldo <easywaldoji@gmail.com>
2022-11-18 23:22:34 +0900

    dsl sample

--- src/test/kotlin/com/example/demo/functional/FunctionalSampleTest.kt
+++ src/test/kotlin/com/example/demo/functional/FunctionalSampleTest.kt
@@ -235,6 +235,19 @@ class FunctionalSampleTest {
         }
         println(html)
     }
+
+    @Test
+    fun dsl_sample() {
+        val commuter = bicycle {
+            description("Fast carbon commuter")
+            bar {
+                barType = BarType.FLAT
+                material = Material.ALUMINIUM
+                this@bicycle.frame { }
+            }
+        }
+        println(commuter)
+    }
 }
 
 fun String.countWords():Int {
@@ -355,4 +368,94 @@ operator fun Wolf.set(relationships: WolfRelationships, wolf: Wolf) {
 
 operator fun Wolf.not() = "$name is angry!!!"
 
+interface Element {
+    fun render(builder: java.lang.StringBuilder, indent: String)
+}
+@DslMarker
+annotation class ElementMarker
 
+@ElementMarker
+abstract class Part(private val name: String) : Element {
+    private val children = arrayListOf<Element>()
+    protected val attributes = hashMapOf<String, String>()
+
+    protected fun <T: Element> initElement(element: T, init: T.() -> Unit): T {
+        element.init()
+        children.add(element)
+        return element
+    }
+
+    override fun render(builder: StringBuilder, indent: String) {
+        builder.append("$indent<$name${renderAttributes()}>\n")
+        children.forEach {
+            c -> c.render(builder, indent + "\t")
+        }
+        builder.append("$indent</$name>\n")
+    }
+
+    private fun renderAttributes(): String = buildString {
+        attributes.forEach {
+            attr, value -> append(" $attr=$value")
+        }
+    }
+
+    override fun toString(): String = buildString{
+        render(this, "")
+    }
+}
+
+enum class Material {
+    CARBON, STEEL, TITANIUM, ALUMINIUM
+}
+
+enum class BarType {
+    DROP, FLAT, TT, BULLHORN
+}
+
+enum class Brake {
+    RIM, DISK
+}
+abstract class PartWithMaterial(name: String) : Part(name) {
+    var material: Material
+        get() = Material.valueOf(attributes["material"]!!)
+        set(value) {
+            attributes["material"] = value.name
+        }
+}
+class Bicycle : Part("bicycle") {
+
+    fun description(description: String) {
+        attributes["description"] = description
+    }
+
+    fun frame(init: Frame.() -> Unit) = initElement(Frame(), init)
+
+    fun fork(init: Fork.() -> Unit) = initElement(Fork(), init)
+
+    fun bar(init: Bar.() -> Unit) = initElement(Bar(), init)
+}
+class Frame : PartWithMaterial("frame") {
+    fun backWheel(init: Wheel.() -> Unit) = initElement(Wheel(), init)
+}
+class Wheel : PartWithMaterial("wheel") {
+    var brake: Brake
+        get() = Brake.valueOf(attributes["brake"]!!)
+        set(value) {
+            attributes["brake"] = value.name
+        }
+}
+class Bar : PartWithMaterial("bar") {
+    var barType: BarType
+        get() = BarType.valueOf(attributes["type"]!!)
+        set(value) {
+            attributes["type"] = value.name
+        }
+}
+class Fork : PartWithMaterial("fork") {
+    fun frontWheel(init: Wheel.() -> Unit) = initElement(Wheel(), init)
+}
+fun bicycle(init: Bicycle.() -> Unit): Bicycle {
+    val cycle = Bicycle()
+    cycle.init()
+    return cycle
+}
