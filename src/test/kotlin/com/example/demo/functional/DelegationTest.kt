8e9d39fc64aa8eb05a907d3269e146de5383aadb
easywaldo <easywaldoji@gmail.com>
2022-11-20 23:51:29 +0900

    Exception raised when value is null

--- src/test/kotlin/com/example/demo/functional/DelegationTest.kt
+++ src/test/kotlin/com/example/demo/functional/DelegationTest.kt
@@ -10,6 +10,8 @@ class DelegationTest {
     fun test() {
         notNullStr = "Initial value"
         println(notNullStr)
+        println(notInit)
     }
 }
 var notNullStr: String by Delegates.notNull<String>()
+var notInit: String by Delegates.notNull<String>()
