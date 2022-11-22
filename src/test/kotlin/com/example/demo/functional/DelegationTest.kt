e46667ed5c9cb8f2aa4974414a2114771dd7bed2
easywaldo <easywaldoji@gmail.com>
2022-11-22 22:45:45 +0900

    Custom delegation

--- src/test/kotlin/com/example/demo/functional/DelegationTest.kt
+++ src/test/kotlin/com/example/demo/functional/DelegationTest.kt
@@ -1,10 +1,13 @@
 package com.example.demo.functional
 
+import com.example.demo.pattern.factory.property
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.properties.Delegates
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
 @SpringBootTest
 class DelegationTest {
@@ -52,6 +55,15 @@ class DelegationTest {
         val book2 =Book(map2)
 
         println("Book1 $book1 nBook2 $book2")
+
+        myEven = 6
+        println("myEven:$myEven")
+        myEven = 3
+        println("myEven:$myEven")
+        myEven = 5
+        println("myEven:$myEven")
+        myEven = 8
+        println("myEven:$myEven")
     }
 }
 var notNullStr: String by Delegates.notNull<String>()
@@ -84,3 +96,28 @@ data class Book(val delegates:Map<String, Any?>) {
     val publisher: String by delegates
 
 }
+
+var myEven: Int by makeEven(0) {
+    property, oldValue, newValue, wasEven ->
+    println("${property.name} $oldValue -> $newValue, Even:$wasEven")
+}
+abstract class MakeEven(initialValue: Int): ReadWriteProperty<Any?, Int> {
+    private var value: Int = initialValue
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Int = value
+    override fun setValue(thisRef: Any?, property: KProperty<*>, newValue: Int) {
+        val oldValue = newValue
+        val wasEven = newValue % 2 == 0
+        if (wasEven) {
+            this.value = newValue
+        } else {
+            this.value = newValue + 1
+        }
+        afterAssignmentCall(property, oldValue, newValue, wasEven)
+    }
+    abstract fun afterAssignmentCall(property: KProperty<*>, oldValue: Int, newValue: Int, wasEven: Boolean): Unit
+}
+inline fun makeEven(initialValue: Int, crossinline onAssignment: (KProperty<*>, oldValue:Int, newValue: Int, wasEven: Boolean)->Unit): ReadWriteProperty<Any?, Int>
+    = object: MakeEven(initialValue) {
+    override fun afterAssignmentCall(property: KProperty<*>, oldValue: Int, newValue: Int, wasEven: Boolean) =
+        onAssignment(property, oldValue, newValue, wasEven)
+    }
