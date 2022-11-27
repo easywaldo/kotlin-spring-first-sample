6849252a0516620706b23af2ddabc9d8f8ad73cd
easywaldo <easywaldoji@gmail.com>
2022-11-27 18:46:19 +0900

    Future sample

--- src/test/kotlin/com/example/demo/webflux/ThreadPoolTest.kt
+++ src/test/kotlin/com/example/demo/webflux/ThreadPoolTest.kt
@@ -2,8 +2,10 @@ package com.example.demo.webflux
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import java.util.concurrent.Callable
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 @SpringBootTest
 class ThreadPoolTest {
@@ -20,5 +22,22 @@ class ThreadPoolTest {
             pool.shutdown()
         }
         println("current-thread-name : ${Thread.currentThread().name}")
+    }
+
+    fun sum(a: Int, b: Int) = a + b
+
+    @Test
+    fun future_test() {
+        // Runnable 이 아닌 Callable 로 사용한다
+        // 한개 이상의 비동기 이상의 작업을 조합하는 데에 있어서 완료,정지.재개 등의 작업을 지원하지 않는다
+        val pool = Executors.newSingleThreadExecutor()
+        val future = pool.submit(Callable {
+            sum(100, 200)
+        })
+
+        println("계산 시작")
+        val futureResult = future.get(5000L, TimeUnit.MILLISECONDS)
+        println(futureResult)
+        println("계산 종료")
     }
 }
