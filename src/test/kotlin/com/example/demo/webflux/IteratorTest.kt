10084bb95f3246d8c78c3f7aa1c31812793453d3
easywaldo <easywaldoji@gmail.com>
2022-11-27 19:13:58 +0900

    Iterator sample

---
+++ src/test/kotlin/com/example/demo/webflux/IteratorTest.kt
@@ -0,0 +1,32 @@
+package com.example.demo.webflux
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IteratorTest {
+    @Test
+    fun test() {
+        val carIterable = CarIterable(listOf(Car("람보르기니"), Car("아이오닉 5")))
+        val iterator = carIterable.iterator()
+
+        while (iterator.hasNext()) {
+            println("브랜드 : ${iterator.next()}")
+        }
+    }
+}
+
+data class Car(val brand: String)
+class CarIterable(val cars: List<Car> = listOf()): Iterable<Car> {
+    override fun iterator(): Iterator<Car> = CarIterator(cars)
+}
+
+class CarIterator(val cars: List<Car> = listOf(), var index : Int = 0): Iterator<Car> {
+    override fun hasNext(): Boolean {
+        return cars.size > index
+    }
+
+    override fun next(): Car {
+        return cars[index++]
+    }
+}
