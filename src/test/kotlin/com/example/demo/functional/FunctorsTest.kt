01ec1d437893ecf1fbc1b9952937cc5f93043aa7
easywaldo <easywaldoji@gmail.com>
2022-12-01 23:41:46 +0900

    define a map

--- src/test/kotlin/com/example/demo/functional/FunctorsTest.kt
+++ src/test/kotlin/com/example/demo/functional/FunctorsTest.kt
@@ -12,4 +12,30 @@ class FunctorsTest {
             .map(Int::toString)
             .forEach(::println)
     }
+
+    @Test
+    fun option_map_test() {
+        val optionSome = Option.Some("Kotlin")
+            .map(String::toUpperCase)
+        val optionNone = Option.None.map(String::toUpperCase)
+
+        println(optionSome)
+        println(optionNone)
+    }
+}
+
+sealed class Option<out T> {
+    object None: Option<Nothing>() {
+        override fun toString(): String {
+            return "None"
+        }
+    }
+    data class Some<out T>(val value: T): Option<T>()
+
+    companion object
+}
+
+fun <T, R> Option<T>.map(transform: (T) -> R): Option<R> = when(this) {
+    Option.None -> Option.None
+    is Option.Some -> Option.Some(transform(value))
 }
