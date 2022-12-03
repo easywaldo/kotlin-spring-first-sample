e1fc7fefcb860abb40986d726dae3023c46c12b9
easywaldo <easywaldoji@gmail.com>
2022-12-03 23:45:26 +0900

    define flatmap

--- src/test/kotlin/com/example/demo/functional/MonadsTest.kt
+++ src/test/kotlin/com/example/demo/functional/MonadsTest.kt
@@ -12,4 +12,27 @@ class MonadsTest {
             .joinToString()
         println(result)
     }
+
+    @Test
+    fun flat_map() {
+        println(calculateDiscount((Option.Some(80.0))))
+        println(calculateDiscount((Option.Some(30.0))))
+        println(calculateDiscount((Option.None)))
+    }
+}
+
+fun <T, R> Option<T>.flatMap(fm: (T) -> Option<R>): Option<R> = when(this) {
+    Option.None -> Option.None
+    is Option.Some -> fm(value)
+}
+
+fun calculateDiscount(price: Option<Double>): Option<Double> {
+    return price.flatMap { p ->
+        if (p > 50.0) {
+            Option.Some(5.0)
+        }
+        else {
+            Option.None
+        }
+    }
 }
