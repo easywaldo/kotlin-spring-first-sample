c3cb37ddeb13ffd8351cb38ce9c83c6b6d3d4d47
easywaldo <easywaldoji@gmail.com>
2022-12-11 23:51:53 +0900

    set dataclass and set customeDataClass

---
+++ src/test/kotlin/com/example/demo/functional/CustomeDataSetTest.kt
@@ -0,0 +1,42 @@
+package com.example.demo.functional
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CustomeDataSetTest {
+    @Test
+    fun test() {
+        val dataClassSet = setOf(
+            MyDataClass(1, "1st obj"),
+            MyDataClass(2, "2st obj"),
+            MyDataClass(3, "3st obj"),
+            MyDataClass(1, "1st obj"),
+        )
+
+        println("Printing items of dataClassSet one by one")
+        for (item in dataClassSet) {
+            println(item)
+        }
+
+        val customeDataClassSet = setOf(
+            MyCustomeClass(1, "1st obj"),
+            MyCustomeClass(2, "2st obj"),
+            MyCustomeClass(3, "3st obj"),
+            MyCustomeClass(1, "1st obj")
+        )
+
+        println("Printing items of customDataClassSet one by one")
+        for (item in customeDataClassSet) {
+            println(item)
+        }
+
+    }
+}
+
+data class MyDataClass (val someNumericValue:Int, val someStringValue:String)
+class MyCustomeClass (val someNumericValue: Int, val someStringValue:String) {
+    override fun toString(): String {
+        return "MyCustomeClass(someNumericValue=$someNumericValue, someStringValue=$someStringValue)"
+    }
+}
