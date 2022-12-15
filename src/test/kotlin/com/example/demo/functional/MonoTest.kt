1104d2acf9be0d2965b43db9a0554f209d793d23
easywaldo <easywaldoji@gmail.com>
2022-12-15 23:14:43 +0900

    Flux sample

--- src/test/kotlin/com/example/demo/functional/MonoTest.kt
+++ src/test/kotlin/com/example/demo/functional/MonoTest.kt
@@ -2,6 +2,7 @@ package com.example.demo.functional
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @SpringBootTest
@@ -11,4 +12,17 @@ class MonoTest {
         val mono: Mono<String> = Mono.just("Hello Reactive World")
         mono.subscribe(::println)
     }
+
+    @Test
+    fun test2() {
+        val iphone = CellPhone(name="iPhone", price = 100, currency = Currency.KRW)
+        val galaxy = CellPhone(name = "Galaxy", price = 90, currency = Currency.KRW)
+        val flux: Flux<CellPhone> = Flux.just(iphone, galaxy)
+        flux.subscribe(::println)
+    }
+}
+
+data class CellPhone(val name: String, val price: Int, val currency: Currency)
+enum class Currency {
+    KRW, USD
 }
