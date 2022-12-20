7dcf23823a3000480ae0b4856e6f90fd99ad5b1c
easywaldo <easywaldoji@gmail.com>
2022-12-21 00:18:58 +0900

    let function sample

--- src/test/kotlin/com/example/demo/guide-kt/RunWithSampleTest.kt
+++ src/test/kotlin/com/example/demo/guide-kt/RunWithSampleTest.kt
@@ -2,7 +2,6 @@ package com.example.demo.`guide-kt`
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-
 @SpringBootTest
 class RunWithSampleTest {
     @Test
@@ -15,14 +14,28 @@ class RunWithSampleTest {
             post("Hello!")
             showCityAddress()
         }
+        Address(123456, "Seoul", "Good Street", "129GO").let {
+            addr -> println("${addr.city}")
+            addr.post("Hello")
+        }
     }
 }
+
 
 class Address {
     var zipCode: Int = 0
     var city: String = ""
     var street: String = ""
     var house: String = ""
+
+    constructor()
+    constructor(zipCode: Int, city: String, street: String, house: String) {
+        this.zipCode = zipCode
+        this.city = city
+        this.street = street
+        this.house = house
+    }
+
 
     fun post(message: String): Boolean {
         println("Message for {$zipCode, $city, $street, $house}: $message")
