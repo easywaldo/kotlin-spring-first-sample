c92f712e5eff5cd83bbbe14632d70ae9b85c7528
easywaldo <easywaldoji@gmail.com>
2022-12-22 23:46:53 +0900

    Enum class constructor

--- src/test/kotlin/com/example/demo/guide-kt/EnumTest.kt
+++ src/test/kotlin/com/example/demo/guide-kt/EnumTest.kt
@@ -10,6 +10,9 @@ class EnumTest {
         val day = WeekDay.TUE
         println(day.isNotWorkDay())
         println(WeekDay.SUN.isNotWorkDay())
+
+        val blue = RainbowColor.BLUE
+        println(blue.isCold)
     }
 }
 
@@ -17,4 +20,16 @@ enum class WeekDay {
     MON, TUE, WED, THU, FRI, SAT, SUN;
     val lowerCaseName get() = name.lowercase()
     fun isNotWorkDay() = this in listOf(SAT, SUN)
+}
+
+enum class RainbowColor(val isCold: Boolean) {
+    RED(false),
+    ORANGE(false),
+    YELLOW(false),
+    GREEN(true),
+    BLUE(true),
+    INDIGO(true),
+    VIOLET(true);
+
+    val isWarm get() = !isCold
 }
