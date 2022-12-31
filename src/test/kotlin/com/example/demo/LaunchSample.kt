8ecc2cf7ec53a2258cdce6f4cb8908879734de17
easywaldo <easywaldoji@gmail.com>
2022-12-31 17:33:37 +0900

    allocation job from launch context

--- src/test/kotlin/com/example/demo/LaunchSample.kt
+++ src/test/kotlin/com/example/demo/LaunchSample.kt
@@ -1,5 +1,6 @@
 package com.example.demo
 
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -12,18 +13,21 @@ class LaunchSample {
     @Test
     fun test() {
         runBlocking {
-            launch {
+            val job1: Job = launch {
                 val elapsedTime = measureTimeMillis {
                     delay(150)
                 }
                 println("async task-1 $elapsedTime ms")
             }
-            launch {
+            val job2: Job = launch {
                 val elapsedTime = measureTimeMillis {
                     delay(100)
                 }
                 println("async task-2 $elapsedTime ms")
             }
+            job1.cancel()
+            println("start -->> task-2")
+            job2.start()
         }
     }
 }
